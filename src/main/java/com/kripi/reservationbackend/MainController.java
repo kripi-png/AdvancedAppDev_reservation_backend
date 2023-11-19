550d7b9e896de4c5e5f13c366e3b528bb6dbfd6f
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-11-19 17:12:34 +0200

    feat: `POST /token` for JWT Token

--- src/main/java/com/kripi/reservationbackend/MainController.java
+++
@@ -1,8 +0,0 @@
-package com.kripi.reservationbackend;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(path="/demo")
-public class MainController {}
