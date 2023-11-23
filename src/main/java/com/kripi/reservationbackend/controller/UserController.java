967bf51cfe91475230fc95dfc80d6680ac27e5c2
kripi-png <66375160+kripi-png@users.noreply.github.com>
2023-11-23 15:18:06 +0200

    style: remove unused /auth endpoints

--- src/main/java/com/kripi/reservationbackend/controller/UserController.java
+++ src/main/java/com/kripi/reservationbackend/controller/UserController.java
@@ -2,18 +2,20 @@ package com.kripi.reservationbackend.controller;
 
 import com.kripi.reservationbackend.config.AuthRequest;
 import com.kripi.reservationbackend.config.UserInfoDetails;
-import com.kripi.reservationbackend.service.JwtService;
 import com.kripi.reservationbackend.model.UserInfo;
+import com.kripi.reservationbackend.service.JwtService;
 import com.kripi.reservationbackend.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
@@ -64,18 +66,6 @@ public class UserController {
         } else {
             throw new UsernameNotFoundException("User not found with credentials");
         }
-    }
-
-    @GetMapping("/user/userProfile")
-    @PreAuthorize("hasAuthority('ROLE_USER')")
-    public String userProfile() {
-        return "Welcome to User Profile";
-    }
-
-    @GetMapping("/admin/adminProfile")
-    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-    public String adminProfile() {
-        return "Welcome to Admin Profile";
     }
 
 }
